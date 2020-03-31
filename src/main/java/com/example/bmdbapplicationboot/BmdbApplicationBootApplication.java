232282c7ed2c5dfb424bac0d636f1b74903934ec
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-31 23:03:06 +0200

    extract initial files from start.spring.io

---
+++ src/main/java/com/example/bmdbapplicationboot/BmdbApplicationBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmdbapplicationboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmdbApplicationBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmdbApplicationBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bmdbapplicationboot/BmdbApplicationBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmdbapplicationboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmdbApplicationBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
