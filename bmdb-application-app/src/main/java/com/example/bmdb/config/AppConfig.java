1e2ec18d224507f161b9f2c15de6cd7b561f68ae
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-04-02 05:28:05 +0200

    Remove createAppSpring use auto config instead

--- bmdb-application-app/src/main/java/com/example/bmdb/config/AppConfig.java
+++ bmdb-application-app/src/main/java/com/example/bmdb/config/AppConfig.java
@@ -1,24 +1,12 @@
 package com.example.bmdb.config;
 
-import com.example.bmdb.app.AppSpring;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 
 
 @Configuration
-@ComponentScan("com.example")
-
 @Import({ MessageConfig.class, DomainMockConfig.class })
 public class AppConfig {
-
-    @Bean
-    public AppSpring createAppSpring(){
-        return new AppSpring();
-    }
 
 }
