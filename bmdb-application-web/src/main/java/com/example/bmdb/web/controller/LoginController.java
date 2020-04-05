b3a565c6c3eadd95552ce047d283bbb9bafa8c11
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-04-05 20:57:58 +0200

    use international error messages

--- bmdb-application-web/src/main/java/com/example/bmdb/web/controller/LoginController.java
+++ bmdb-application-web/src/main/java/com/example/bmdb/web/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.bmdb.web.controller;
 
 import com.example.bmdb.services.auth.SecurityService;
+import com.example.bmdb.view.I18nMsg;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +17,19 @@ public class LoginController {
     @Inject
     private SecurityService securityService;
 
+    private I18nMsg i18nMsg;
+    @Inject
+    public void setI18nMsg(I18nMsg i18nMsg) {
+        this.i18nMsg = i18nMsg;
+    }
+
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         if (error != null)
-            model.addAttribute("error", "Your email or password is invalid.");
+            model.addAttribute("error", i18nMsg.getMsg("emailOrPassInvalid"));
 
         if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
+            model.addAttribute("message", i18nMsg.getMsg("success.logout"));
 
         return "login";
     }
